fd61f98ba9519d75dc7fa148d6119d186e00eabe
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-08-03 15:55:59 -0600

    Add modified arrays

--- friends.js
+++ friends.js
@@ -2,3 +2,10 @@ var arrays = ["Tim", "Brett", "James", "Dmitrii", "Kaitlyn"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var places = ["New York", "Miami", "Denver", "Burlington", "Chicago"]
+places.unshift("Seattle")
+console.log(places)
+
+places.push("Las Vegas")
+console.log(places)
